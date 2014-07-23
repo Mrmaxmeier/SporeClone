bb8fa4ca23c68cbe61a60e547f242446cddcbd57
Mrmaxmeier <Mrmaxmeier@gmail.com>
2014-07-23 15:29:10 +0200

    cx_freeze Executable

---
+++ build.py
@@ -0,0 +1,20 @@
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+buildOptions = dict(packages=["OpenGL"],
+					include_files=['data/'],
+					excludes=['tkinter']
+					)
+
+base = None
+
+executables = [
+	Executable('creaturecreator.py', base=base)
+]
+
+setup(name='Sp0re',
+	  version='0.1',
+	  description='THE Spore CLONE!!!oneoneone',
+	  options=dict(build_exe=buildOptions),
+	  executables=executables)
