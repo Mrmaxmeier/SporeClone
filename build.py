70c970cd483afb5aa93ed8a88882ac65c3784bd3
Mrmaxmeier <Mrmaxmeier@gmail.com>
2014-07-28 18:34:47 +0200

    wierd build stuff

--- build.py
+++ build.py
@@ -1,21 +1,37 @@
+import sys
 from cx_Freeze import setup, Executable
+print()
+print("#" * 15)
+print()
+print('Building on', sys.platform)
+print()
+print("#" * 15)
+print()
 
 # Dependencies are automatically detected, but it might need
 # fine tuning.
-buildOptions = dict(packages=["OpenGL"],
+buildOptions = dict(packages=["OpenGL", "tkinter", "tkinter.filedialog"],
 					include_files=['data/'],
-					excludes=['tkinter'],
-					icon="icon.icns"
+					excludes=[],
+					icon="icon.png"
 					)
 
 base = None
 
-executables = [
-	Executable('creaturecreator.py', base=base)
-]
+executables = [Executable('creaturecreator.py', base=base)]
+#executables = [Executable('main.py', base=base)]
+#executables = [Executable('tktest.py', base=base)]
+
+#if sys.platform == 'darwin':
+#	bundle = dict(iconfile="icon.icns")
+#	dmg = dict(volume_label="Sp0re Disk Image")
+#	options = dict(build_exe=buildOptions, build_mac=bundle, build_dmg=dmg)
+#else:
+#	options = dict(build_exe=buildOptions)
+options = dict(build_exe=buildOptions)
 
 setup(name='Sp0re',
-	  version='0.1',
-	  description='THE Spore CLONE!!!oneoneone',
-	  options=dict(build_exe=buildOptions),
-	  executables=executables)
+	version='0.1',
+	description='THE Spore CLONE!!!oneoneone',
+	options=options,
+	executables=executables)
