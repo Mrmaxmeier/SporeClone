0d5cfcf514d2aa9f45dd972e250c5c66eca04272
Mrmaxmeier <Mrmaxmeier@gmail.com>
2014-07-31 08:51:13 +0200

    new own server

--- server.py
+++ server.py
@@ -7,10 +7,11 @@ from player import ArtificialPlayer, NetworkPlayer
 
 #Serverstuffs
 
-import socketserver
-import json
 import supersocket
-
+import json
+import threading
+import socket
+import queue
 
 sampleMsg = {'joined': {'desc': 'Is called on Connection', 'name': 'Mrmaxmeier', 'body': {'desc': 'BodyDict from data/creatures'}},
 	'u': {'desc': 'Used to update PlayerVars', 'pos': [10, 10]},
@@ -25,11 +26,65 @@ sampleMsg = {'joined': {'desc': 'Is called on Connection', 'name': 'Mrmaxmeier',
 #
 
 
-class TCPServer(socketserver.ThreadingTCPServer):
-	allow_reuse_address = True
+class SimpleServer(threading.Thread):
+	def __init__(self, ip, port, newThreadHandler):
+		threading.Thread.__init__(self)
+		self.setDaemon(True)
+		self.newThreadHandler = newThreadHandler
+
+		server_address = (ip, port)
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		self.sock.bind(server_address)
+		self.server_address = self.sock.getsockname()
+		print('Server bound to ', self.server_address)
+		self.sock.listen(5)
+		self.isAlive = True
+
+		self.connectedClients = []
+		self.name2thread = {}
+
+		self.start()
+
+	def serve(self):
+		while self.isAlive:
+			(clientsock, (ip, port)) = self.sock.accept()
+			print('New Connection @', ip, ':', port)
+			sendToClientQueue = queue.Queue()
+			newThread = self.newThreadHandler(clientsock, sendToClientQueue, self)
+			self.connectedClients.append(newThread)
+
+	def sendToName(self, name, msg, sendDirect=True):
+		if name in self.name2thread:
+			self.name2thread[name].queue.put(msg)
+			if sendDirect:
+				self.name2thread.processQueue()
+			return True
+		else:
+			print(name, 'not in name2queue')
+			return False
+
+	def sendToAll(self, msg):
+		for thread in self.connectedClients:
+			thread.sendQueue.put(msg)
+			thread.processQueue()
+
+	def registerName(self, name, thread):
+		self.name2thread[name] = thread
 
 
-class ServerHandler(socketserver.BaseRequestHandler):
+class ClientHandlerThread(threading.Thread):
+	def __init__(self, clientsock, sendQueue, server):
+		threading.Thread.__init__(self)
+		self.sock = clientsock
+		self.sendQueue = sendQueue
+		self.supersock = supersocket.SuperSocket(clientsock)
+		self.server = server
+		self.start()
+
+	def run(self):
+		print('Thread started')
+		self.handle()
 
 	def handleData(self, d):
 		return {'return': 'no_response'}
@@ -37,8 +92,7 @@ class ServerHandler(socketserver.BaseRequestHandler):
 	def handle(self):
 		try:
 			while 1:
-				supersock = supersocket.SuperSocket(self.request)
-				bdata = supersock.recv()
+				bdata = self.supersock.recv()
 				if not bdata:
 					#if bdata == b'':
 					print('Connection Closed!')
@@ -51,11 +105,23 @@ class ServerHandler(socketserver.BaseRequestHandler):
 				# send some 'ok' back
 				resp = json.dumps(response)
 				#respBytes = bytes(resp, 'UTF-8')
-				supersock.send(resp)
+				self.supersock.send(resp)
+
+				self.processQueue()
 		except Exception as e:
-			print("Exception wile receiving message: ", e)
+			print(type(e), "while receiving message: ", e)
+
+	def processQueue(self):
+		while True:
+			try:
+				msg = self.sendQueue.get_nowait()
+				self.supersock.send(msg)
+			except queue.Empty:
+				break
+			except Exception as e:
+				print(type(e), "while sending queued message: ", e)
 
 
 if __name__ == "__main__":
-	server = TCPServer(('0.0.0.0', 13373), ServerHandler)
-	server.serve_forever()
+	server = SimpleServer('0.0.0.0', 13373, ClientHandlerThread)
+	server.serve()
